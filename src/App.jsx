4accfe3689a27292dbede3ac89155e5978f2a634
Yann Kamche <yannkamche@gmail.com>
2024-03-07 02:01:52 +0100

    completed the shop section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './sections/Navbar/Navbar'
 import Home from './sections/Home/Home'
 import Collection from './sections/Collection/Collection'
-
+import Shop from './sections/Shop/Shop'
+import { shop } from './constants/constants'
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
       <Collection />
+      <Shop shop={shop}/>
     </>
   )
 }
